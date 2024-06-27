76362a80298f3900a0ab5533c9a2f576a0fbdd50
FishTub <alif.nurrokh@gmail.com>
2024-06-27 16:19:27 +1200

    Added new success route for the user to be redirected to after they have successfully uploaded the files

--- app.py
+++ app.py
@@ -6,5 +6,19 @@ app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
 def file_upload():
+    # if request.method == "POST":
+    #     if 'file' not in request.files:
+    #         return "No files uploaded", 400
+    #     files = request.files.getlist("files[]")
+    #     for file in files:
+    #         if file.filename == "":
+    #             return "No files selected", 400
+            
+    #         file.save("uploads/" + file.filename)       
+
+    
     return render_template('index.html')
 
+@app.route("/success", methods=["GET", "POST"])
+def sucess():
+    return render_template('success.html')
